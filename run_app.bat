59dbf5d9287a8943f8c34ef3ca38d44dece20c79
Abderrahmane Mejdoubi <abderrahmane.mejdoubi23@ump.ac.ma>
2026-05-05 14:26:24 +0100

    Delete run_app.bat

--- run_app.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0PaieMultiSocietesPro2025"
-echo Starting Paie Multi-Societes WPF Final Corrected Pro...
-dotnet restore
-dotnet build
-if %errorlevel% neq 0 pause & exit /b %errorlevel%
-dotnet run
-pause
